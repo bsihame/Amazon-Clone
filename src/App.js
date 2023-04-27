15c940b80b4ba572dd84882840c013e67195bfbf
bsihame <bsihame@hotmail.com>
2023-04-27 18:19:14 -0400

    import updated packages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 // import Home from "./Pages/Home";
 import Checkout from "./Pages/Checkout";
 import Login from "./Pages/Login";
